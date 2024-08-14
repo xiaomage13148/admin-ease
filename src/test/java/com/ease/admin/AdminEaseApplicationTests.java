86924bce85267c18d490470c9f3ebdbfb7b8f74c
majh8 <majh8@chinatelecom.cn>
2024-08-14 14:30:42 +0800

    init: 初始化项目

---
+++ src/test/java/com/ease/admin/AdminEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ease.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
