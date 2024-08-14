e264c534ad7ccf678a63bc4346d240b95ccd8b95
xiaomage <15234829838@163.com>
2024-08-14 16:16:03 +0800

    feat: 项目搭建

--- src/main/java/com/ease/admin/AdminEaseApplication.java
+++ src/main/java/com/ease/admin/AdminEaseApplication.java
@@ -1,13 +1,42 @@
 package com.ease.admin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication
+@ServletComponentScan
+@MapperScan({"com.ease.admin.mapper"})
+@EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class AdminEaseApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AdminEaseApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(AdminEaseApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isEmpty(path)) {
+            path = "";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application  is running! Access URLs:\n\t" +
+                "Local访问网址: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External访问网址: \thttp://" + ip + ":" + port + path + "\n\t" +
+                "doc访问地址: \t\thttp://localhost:" + port + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }
