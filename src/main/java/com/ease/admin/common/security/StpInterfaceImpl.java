1e4e972c4d9ca13807519cb853623458b02da15c
xiaomage <15234829838@163.com>
2024-08-16 17:40:19 +0800

    fix: 权限认证实现类

---
+++ src/main/java/com/ease/admin/common/security/StpInterfaceImpl.java
@@ -0,0 +1,28 @@
+package com.ease.admin.common.security;
+
+import cn.dev33.satoken.stp.StpInterface;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 权限认证实现类
+ * TODO 待完善..
+ * Description: <br/>
+ *
+ * @author mjh8 <br/>
+ * @date: 2024/8/16 下午5:39 <br/>
+ * @since JDK 17
+ */
+@Component
+public class StpInterfaceImpl implements StpInterface {
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+        return List.of();
+    }
+
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        return List.of();
+    }
+}
